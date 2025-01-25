cdb4fd0c44cb3557ebcd5c24eac6451394cb4259
Matthew Martin <matthewdeanmartin@gmail.com>
2025-01-24 21:24:14 -0500

    Create longest_substring.py

---
+++ coding_challenges/longest_substring.py
@@ -0,0 +1,12 @@
+# Longest Substring Without Repeating Characters Difficulty: Medium
+# Given a string s, find the length of the longest substring without repeating characters.
+
+# Example: Input: "abcabcbb" 
+# Output: 3 (The answer is "abc")
+
+# Once you solve this, some companies often extend the problem to further test your thinking:
+
+# Extension 1: What if the string is very large (millions of characters)? How would you optimize your solution to handle that efficiently?
+
+# Extension 2: Now, modify the problem: Find the longest substring where at most k characters can repeat. How would you solve that?
+# source: https://www.linkedin.com/posts/muskan-chaudhary_heres-a-popular-question-asked-at-top-companies-activity-7287390896713584640-eWbl?utm_source=share&utm_medium=member_desktop
